fbc534631852691d9eec8fcc8831ec19d0367e49
Arunima484 <76149744+Arunima484@users.noreply.github.com>
2023-02-11 17:28:15 +0530

    Patch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-// import TodoComponent from './components/TodoComponent';
-import TodoFormComponent from './components/TodoFormComponent';
+ import TodoComponent from './components/TodoComponent';
+//import TodoFormComponent from './components/TodoFormComponent';//
 
 function App() {
   return (
     <div className="App">
-     {/* <TodoComponent></TodoComponent> */}
-     <TodoFormComponent></TodoFormComponent>
+     <TodoComponent></TodoComponent>
+      {/*<TodoFormComponent></TodoFormComponent> */}
     </div>
   );
 }
